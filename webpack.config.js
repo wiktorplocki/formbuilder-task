ff80498c41724f7c27021deb9b0254483d31a317
Wiktor Płocki <wiktor.plocki@gmail.com>
2018-11-05 11:33:20 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/App.js',
+  output: {
+    filename: 'main.[hash].js',
+    chunkFilename: '[name].[hash].js'
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'initial',
+      name: true
+    }
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: [['@babel/preset-env'], '@babel/preset-react']
+        }
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { importLoaders: 2, sourceMap: true }
+          },
+          { loader: 'sass-loader' }
+        ]
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            name: '[path][name]-[hash].[ext]'
+          }
+        }
+      },
+      {
+        test: /\.(ttf|eof|woff|woff2)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'fonts/[name].[ext]'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.[hash].css',
+      chunkFilename: '[name].styles.[hash].css'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+};
